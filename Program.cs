f2812c6eb48b56f7e0d62032f2a4d7999137e947
Dave Baskin <dfbaskin@gmail.com>
2020-09-28 14:12:05 -0400

    Adding expression.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AnalysisServices.Tabular;
 
 namespace PowerBIUtil
 {
@@ -8,15 +9,33 @@ namespace PowerBIUtil
         {
             try
             {
-                if(args.Length < 1 || !Int32.TryParse(args[0], out int portNum))
+                if (args.Length < 1 || !Int32.TryParse(args[0], out int portNum))
                 {
                     throw new ArgumentException("Port number must be specified.");
                 }
 
                 Console.WriteLine($"Working with Power BI on port {portNum}");
 
+                var server = new Server();
+                server.Connect($"localhost:{portNum}");
+
+                // System.Diagnostics.Debugger.Launch();
+
+                var model = server.Databases[0].Model;
+                foreach (var table in model.Tables)
+                {
+                    Console.WriteLine($"Table: {table.Name}");
+                    foreach (var part in table.Partitions)
+                    {
+                        Console.WriteLine($"    Partition: {part.Name} ({part.SourceType})");
+                        if (part.Source is MPartitionSource src)
+                        {
+                            Console.WriteLine(src.Expression);
+                        }
+                    }
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine($"{ex.GetType().FullName} - {ex.Message}");
             }
